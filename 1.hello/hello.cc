1c8fe3694cf3a5efeab68270380f7c5014d28ebb
Maksym V <maxsdtgv@gmail.com>
2017-11-05 22:13:14 +0200

    Description added

--- 1.hello/hello.cc
+++ 1.hello/hello.cc
@@ -1,10 +1,12 @@
 // 'Hello World!' program 
  
-#include <iostream>
+#include <iostream> // Prerocessing derective from standard library 
  
-int main()
-{
-  std::cout << "Hello World!" << std::endl;
+int main() // main function
+{          // start body of the main function  
+  std::cout << "Hello World!" << std::endl; // << called insertion operator
   std::cout << "Hello me 1!" << std::endl;
+  std::cout << std::endl;
+
   return 0;
 }
